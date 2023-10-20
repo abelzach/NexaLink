5cc13df48e1a7491eb831740c93a16f51fb8d628
abelzach <abelsimonz@gmail.com>
2023-10-20 10:30:53 +0530

    Puzzle logic added

--- frontend/pages/puzzle8.jsx
+++ frontend/pages/puzzle8.jsx
@@ -9,23 +9,50 @@ class Puzzle extends Component {
     };
   }
 
+
+  // updatePosition = (index) => {
+  //   let { positions } = this.state;
+  //   let emptyIndex = positions.indexOf(0);
+  //   let targetIndex = positions.indexOf(index);
+  //   const dif = Math.abs(targetIndex - emptyIndex);
+
+  //   if (dif === 1 || dif === 3) {
+  //     positions[emptyIndex] = index;
+  //     positions[targetIndex] = 0;
+  //     this.setState({ positions });
+
+  //     let win = _.every(positions, (value, index, array) => {
+  //       value = value || 9;
+  //       return index === 0 || parseInt(array[index - 1]) <= parseInt(value);
+  //     });
+
+  //     if (win) {
+  //       this.props.onGameWin(true);
+  //       window.alert('You Win!!!');
+  //     }
+  //   }
+  // };
   updatePosition = (index) => {
     let { positions } = this.state;
     let emptyIndex = positions.indexOf(0);
     let targetIndex = positions.indexOf(index);
     const dif = Math.abs(targetIndex - emptyIndex);
-
+  
     if (dif === 1 || dif === 3) {
       positions[emptyIndex] = index;
       positions[targetIndex] = 0;
       this.setState({ positions });
-
-      let win = _.every(positions, (value, index, array) => {
-        value = value || 9;
-        return index === 0 || parseInt(array[index - 1]) <= parseInt(value);
-      });
-
-      if (win) {
+      console.log('Position :: ',positions);
+  
+      // Define the correct order for the puzzle
+      const correctOrder = [1,2,3,4,5,6,7,8,0];
+      console.log('Correct order :: ',correctOrder);
+  
+      // Check if the current positions match the correct order
+      const isGameWon = _.isEqual(positions, correctOrder);
+  
+      if (isGameWon) {
+        this.props.onGameWin(true);
         window.alert('You Win!!!');
       }
     }
